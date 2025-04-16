3df0626d92afc38f0d4dfe55c706c2094309ed40
Ishit Subhram <ishit21csu041@ncuindia.edu>
2025-04-16 21:44:17 +0530

    Added Home Page, Mobile Navbar

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-center h-screen">
+      <h1 className="h1">
+        STOREit
+      </h1>
+    </div>
+  );
+}
