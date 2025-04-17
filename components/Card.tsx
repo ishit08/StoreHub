1ecb47982280f416dc986e5a93e1d9056d91cbac
Ishit Subhram <ishit21csu041@ncuindia.edu>
2025-04-17 16:36:45 +0530

    Added action dropdown with full functionality (rename, view details, share, download, and delete)

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Thumbnail from "@/components/Thumbnail";
 import { convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "@/components/FormattedDateTime";
-import ActionDropdown from "./ActionDropDown";
+import ActionDropdown from "./ActionDropdown";
 
 
 const Card = ({ file }: { file: Models.Document }) => {
@@ -19,7 +19,7 @@ const Card = ({ file }: { file: Models.Document }) => {
         />
 
         <div className="flex flex-col items-end justify-between">
-          {/* <ActionDropdown file={file} /> */}
+          <ActionDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
